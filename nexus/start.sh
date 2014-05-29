f4e4f45afdedbb6d0907ed5e639b9c3b8920c854
saden1 <saden1@gmail.com>
2014-05-29 05:31:24 -0700

    added nexus server

---
+++ nexus/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+NAME="${PWD##*/}"
+
+CONTAINER=$(docker ps -a | awk '{print $NF}'  | grep "$NAME$")
+RUNNING=$?
+
+if [ $RUNNING -eq 0 ]; then
+    echo "$NAME is already running"
+else
+  if [ -z "$HOST_DIR" ] ; then
+    HOST_DIR=/tmp/containers
+    mkdir -p $HOST_DIR/$NAME
+  fi
+
+  echo "Starting $NAME"
+  echo "Mounting Volume $HOST_DIR/$NAME"
+  docker run -d -P -p 8081:8081 -v $HOST_DIR/$NAME:/out -h $NAME --name="$NAME" fitbur/$NAME
+fi
+
